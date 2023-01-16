7320fd2b3186ee6663d2e0d5889a885ab7119aba
cheezedoodles <hougvapes@gmail.com>
2023-01-16 18:17:24 +0300

    require()

--- module_loader.js
+++ module_loader.js
@@ -1,7 +1,31 @@
 function loadModule (filename, module, reqiure) {
-    const wrappedSrc = 
-        `(function (module, exports, require) {
-            ${fs.readFileSync(filename, 'utf8')}
-        })(module, module.exports, require)`
-    eval(wrappedSrc)
+  const wrappedSrc = 
+    `(function (module, exports, require) {
+      ${fs.readFileSync(filename, 'utf8')}
+    })(module, module.exports, require)`
+  eval(wrappedSrc)
+  }
+
+function require(moduleName) {
+  console.log(`Require invoked for module: ${moduleName}`)
+  const id = require.resolve(moduleName)
+  if (require.cache[id]) {
+    return require.cache[id].exports
+  }
+
+  const module = {
+    exports: {},
+    id
+  }
+
+  require.cache[id] = module
+
+  loadModule(id, module, reqiure)
+
+  return module.exports
+}
+
+require.cache = {}
+require.resolve = (moduleName) => {
+  /* resolve a full module id from the moduleName */
 }
