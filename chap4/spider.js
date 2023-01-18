bead9d8b1437d587655dd50532691b9bd3798f1e
cheezedoodles <hougvapes@gmail.com>
2023-01-18 22:12:37 +0300

    small fix of spider.js

--- chap4/spider.js
+++ chap4/spider.js
@@ -32,7 +32,7 @@ function download(url, filename, cb) {
 export function spider (url, cb) {
   const filename = urlToFilename(url)
   fs.access(filename, err => {
-    if (err && err.code === 'ENOENT') {
+    if (!err || err.code !== 'ENOENT') {
       return cb(null, filename, false)
     }
     download(url, filename, err => {
