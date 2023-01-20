fd07a4fd5bb60306c3973b367a9869d77b62e599
cheezedoodles <hougvapes@gmail.com>
2023-01-20 22:43:43 +0300

    made spider async

--- chap5/spider_async.js
+++ chap5/spider_async.js
@@ -28,3 +28,19 @@ async function spiderLinks(currentUrl, content, nesting) {
     await spider(link, nesting - 1)
   }
 }
+
+export async function spider(url, nesting) {
+  const filename = urlToFilename(url)
+  let content
+  try {
+    content = await fsPromises.readFile(filename, 'utf8')
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      throw err
+    }
+    
+    content = await download(url, filename)
+  }
+
+  return spiderLinks(url, content, nesting)
+}
