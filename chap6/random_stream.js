322d381612109a65a7a55d1d45e013af702bbe7e
cheezedoodles <hougvapes@gmail.com>
2023-01-21 22:46:35 +0300

    simplified random_stream.js

--- chap6/random_stream.js
+++ chap6/random_stream.js
@@ -2,19 +2,15 @@ import { Readable } from 'stream'
 import Chance from 'chance'
 
 const chance = new Chance()
+let emittedBytes = 0
 
-export class RandomStream extends Readable {
-  constructor(options) {
-    super(options)
-    this.emittedBytes = 0
-  }
-
-  _read(size) {
-    const chunk = chance.string({ length: size })
+const randomStream = new Readable({
+  read(size) {
+    const chunk = chance.string({length: size})
     this.push(chunk, 'utf8')
-    this.emittedBytes += chunk.length
-    if (chance.bool({ likelihood: 5})) {
+    emittedBytes += chunk.length
+    if (chance.bool({likelihood: 5})) {
       this.push(null)
     }
   }
-}
+})
