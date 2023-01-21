c1a1db127276d6d8d6a71dc3ce63b9ec55d71f69
cheezedoodles <hougvapes@gmail.com>
2023-01-21 18:30:47 +0300

    created gzip-stream

--- chap6/gzip-buffer.js
+++ chap6/gzip-buffer.js
@@ -6,10 +6,10 @@ const gzipPromise = promisify(gzip)
 const filename = process.argv[2]
 
 async function main() {
-    const data = await fs.readFile(filename)
-    const gzippedData = await gzipPromise(data)
-    await fs.writeFile(`${filename}.gz`, gzippedData)
-    console.log('File successfully compressed')
+	const data = await fs.readFile(filename)
+	const gzippedData = await gzipPromise(data)
+	await fs.writeFile(`${filename}.gz`, gzippedData)
+	console.log('File successfully compressed')
 }
 
 main()
