2d1228379a62fbcc582c8590fb1468db9a856d35
cheezedoodles <hougvapes@gmail.com>
2023-01-21 18:37:48 +0300

    small fix in gzipstream

--- chap6/gzip-stream.js
+++ chap6/gzip-stream.js
@@ -5,5 +5,5 @@ const filename = process.argv[2]
 
 createReadStream(filename)
 	.pipe(createGzip())
-	.pipe(createWriteStream(`$(filename).gz`))
+	.pipe(createWriteStream(`${filename}.gz`))
 	.on('finish', () => console.log('File successfully compressed'))
