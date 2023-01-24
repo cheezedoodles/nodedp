59f51fca8e8137e33673eada4e6b8dd6b0b85729
cheezedoodles <hougvapes@gmail.com>
2023-01-24 16:04:55 +0300

    refactored db.js

--- chap7/db.js
+++ chap7/db.js
@@ -1,9 +1,6 @@
-import { dirname, join } from 'path'
-import { fileURLToPath } from 'url'
 import sqlite3 from 'sqlite3'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-export const db = new sqlite3.Database(
-    join(__dirname, 'data.sqlite')
-)
 
+export function createDb(dbFile) {
+  return new sqlite3.Database(dbFile)
+}
