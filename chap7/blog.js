a68445dcf799da8fa6abbaea109c0b281bc6c447
cheezedoodles <hougvapes@gmail.com>
2023-01-24 15:51:51 +0300

    created a blog module

--- chap7/blog.js
+++ chap7/blog.js
@@ -4,4 +4,25 @@ import { db } from './db.js'
 const dbRun = promisify(db.run.bind(db))
 const dbAll = promisify(db.all.bind(db))
 
-export class Blog {}
+export class Blog {
+  initialize() {
+    const initQuery = `CREATE TABLE IF NOT EXISTS posts (
+      id TEXT PRIMARY KEY,
+      title TEXT NOT NULL,
+      content TEXT,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );`
+
+    return dbRun(initQuery)
+  }
+
+  createPost(id, title, content, created_at) {
+    return dbRun('INSERT INTO posts VALUES (?, ?, ?, ?)',
+      id, title, content, created_at
+    )
+  }
+
+  getAllPosts() {
+    return dbAll('SELECT * FROM posts ORDER BY created_at DESC')
+  }
+}
