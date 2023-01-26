2d8ba7f56ed4ddaf43415b8a2c7c227bb3895e8a
cheezedoodles <hougvapes@gmail.com>
2023-01-26 15:40:00 +0300

    created failsafeSocket.js

--- chap9/strategies.js
+++ chap9/strategies.js
@@ -1,11 +1,11 @@
 import ini from 'ini'
 
 export const iniStrategy = {
-    deserialize: data => ini.parse(data),
-    serialize: data => ini.stringify(data)
+  deserialize: data => ini.parse(data),
+  serialize: data => ini.stringify(data)
 }
 
 export const jsonStrategy = {
-    deserialize: data => JSON.parse(data),
-    serialize: data => JSON.stringify(data, null, ' ')
+  deserialize: data => JSON.parse(data),
+  serialize: data => JSON.stringify(data, null, ' ')
 }
