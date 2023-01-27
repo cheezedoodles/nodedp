17082e0be4e4083cb61cf0415b2a42780a80b7f2
cheezedoodles <hougvapes@gmail.com>
2023-01-27 21:31:36 +0300

    refactored checkUrls to implement async generator

--- chap9/checkUrls.js
+++ chap9/checkUrls.js
@@ -5,31 +5,15 @@ export class CheckUrls {
     this.urls = urls
   }
 
-  [Symbol.asyncIterator]() {
-    const urlsIterator = this.urls[Symbol.iterator]()
-
-    return {
-      async next() {
-        const iteratorResult = urlsIterator.next()
-        if (iteratorResult.done) {
-          return { done: true }
-        }
-
-        const url = iteratorResult.value
-        try {
-          const checkResult = await superagent
-            .head(url)
-            .redirects(2)
-          return {
-            done: false,
-            value: `${url} is up, status: ${checkResult.status}`
-          }
-        } catch (err) {
-          return {
-            done: false,
-            value: `${url} is down, error: ${err.message}`
-          }
-        }
+  async * [Symbol.asyncIterator]() {
+    for (const url of this.urls) {
+      try {
+        const checkResult = await superagent
+          .head(url)
+          .redirects(2)
+        yield `${url} is up, status: ${checkResult.status}`
+      } catch (err) {
+        yield `${url} is down, error: ${err.message}`
       }
     }
   }
