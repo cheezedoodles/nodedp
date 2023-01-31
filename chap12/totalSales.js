3640f6f5dddf854d2429575098360d5d4a3e7d0a
cheezedoodles <hougvapes@gmail.com>
2023-01-31 15:46:12 +0300

    created totalSales.js

---
+++ chap12/totalSales.js
@@ -0,0 +1,16 @@
+import level from 'level'
+import subLevel from 'subleveldown'
+
+const db = level('example-db')
+const salesDb = subLevel(db, 'sales', { valueEncoding: 'json' })
+
+export async function totalSales(product) {
+  const now = Date.now()
+  let sum = 0
+  for await (const transaction of salesDb.createValueStream()) {
+    if (!product || transaction.product === product) {
+      sum += transaction.amount
+    }
+  }
+  console.log(`totalSales() took: ${Date.now() - now}ms`)
+}
