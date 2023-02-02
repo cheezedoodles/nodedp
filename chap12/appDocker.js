af88e5e80bf074863cb18c8b5ed4712376f74881
cheezedoodles <hougvapes@gmail.com>
2023-02-02 22:19:00 +0300

    created a dockerifle

--- chap12/appDocker.js
+++ chap12/appDocker.js
@@ -1,10 +1,10 @@
 import { createServer } from "http";
 import { hostname } from "os";
 
-const version = 1;
+const version = 1
 const server = createServer((req, res) => {
   let i = 1e7; while (i > 0) { i-- }
-  res.end(`Hello from ${hostname} (v${version})`)
+  res.end(`Hello from ${hostname()} (v${version})`)
 })
 
 server.listen(8080)
