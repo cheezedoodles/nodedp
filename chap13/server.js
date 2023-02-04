3c3255fccb495a17ff12c43e0cc53df34b5a80f6
cheezedoodles <hougvapes@gmail.com>
2023-02-04 22:08:12 +0300

    modified server.js to leverage amqp

--- chap13/server.js
+++ chap13/server.js
@@ -1,52 +1,57 @@
 import { createServer } from 'http'
 import staticHandler from 'serve-handler'
 import ws from 'ws'
-import yargs from 'yargs'
-import zmq from 'zeromq'
+import JSONStream from 'JSONStream'
+import amqp from 'amqplib'
+import superagent from 'superagent'
 
-// serve static files
-const server = createServer((req, res) => {
-  return staticHandler(req, res, { public: 'www' })
-})
+const httpPort = process.argv[2] || 8080
 
-
-let pubSocket
-async function initializeSockets() {
-  pubSocket = new zmq.Publisher()
-  await pubSocket.bind(`tcp://127.0.0.1:${yargs.argv.pub}`)
-
-  const subSocket = new zmq.Subscriber()
-  const subPorts = [].concat(yargs.argv.sub)
-  for (const port of subPorts) {
-    console.log(`Subscribing to ${port}`)
-    subSocket.connect(`tcp://127.0.0.1:${port}`)
-  }
-  subSocket.subscribe('chat')
-
-  for await (const [msg] of subSocket) {
-    console.log(`Message from another server: ${msg}`)
-    broadcast(msg.toString().split(' ')[1])
-  }
-}
-
-initializeSockets()
-
-const wss = new ws.Server({ server })
-wss.on('connection', client => {
-  console.log('Client connected')
-  client.on('message', msg => {
-    console.log(`Message: ${msg}`)
+async function main() {
+  const connection = await amqp.connect('amqp://localhost')
+  const channel = await connection.createChannel()
+  await channel.assertExchange('chat', 'fanout')
+  const { queue } = await channel.assertQueue(
+    `chat_srv_${httpPort}`,
+    { exclusive: true }
+  )
+  await channel.bindQueue(queue, 'chat')
+  channel.consume(queue, msg => {
+    msg = msg.content.toString()
+    console.log(`From queue: ${msg}`)
     broadcast(msg)
-    pubSocket.send(`chat ${msg}`)
-  })
-})
+  }, { noAck: true }) 
 
-function broadcast(msg) {
-  for (const client of wss.clients) {
-    if (client.readyState === ws.OPEN) {
-      client.send(msg)
+    // serve static files
+  const server = createServer((req, res) => {
+    return staticHandler(req, res, { public: 'www' })
+  })
+
+
+  const wss = new ws.Server({ server })
+  wss.on('connection', client => {
+    console.log('Client connected')
+    client.on('message', msg => {
+      console.log(`Message: ${msg}`)
+      channel.publish('chat', '', Buffer.from(msg))
+    })
+  })
+
+  superagent
+    .get('http://localhost:8090')
+    .on('error', err => console.error(err))
+    .pipe(JSONStream.parse('*'))
+    .on('data', msg => client.send(msg))
+
+  function broadcast(msg) {
+    for (const client of wss.clients) {
+      if (client.readyState === ws.OPEN) {
+        client.send(msg)
+      }
     }
   }
+
+  server.listen(httpPort)
 }
 
-server.listen(yargs.argv.http || 8080)
+main().catch(err => console.error(err))
