da1d3305ede2e6043262d09f7ee52b4fecd4d774
cheezedoodles <hougvapes@gmail.com>
2023-02-04 19:56:38 +0300

    using redis as a msg broker

--- chap13/server.js
+++ chap13/server.js
@@ -1,6 +1,10 @@
 import { createServer } from 'http'
 import staticHandler from 'serve-handler'
 import ws from 'ws'
+import Redis from 'ioredis'
+
+const redisSub = new Redis()
+const redisPub = new Redis()
 
 // serve static files
 const server = createServer((req, res) => {
@@ -12,16 +16,17 @@ wss.on('connection', client => {
   console.log('Client connected')
   client.on('message', msg => {
     console.log(`Message: ${msg}`)
-    broadcast(msg)
+    redisPub.publish('chat_messages', msg)
   })
 })
 
-function broadcast (msg) {
+redisSub.subscribe('chat_messages')
+redisSub.on('message', (channel, msg) => {
   for (const client of wss.clients) {
     if (client.readyState === ws.OPEN) {
       client.send(msg)
     }
   }
-}
+})
 
 server.listen(process.argv[2] || 8080)
