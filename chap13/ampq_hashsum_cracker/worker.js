cb2b478580bca981a2f86f768122738f91ed32e1
cheezedoodles <hougvapes@gmail.com>
2023-02-05 20:54:23 +0300

    created a worker

---
+++ chap13/ampq_hashsum_cracker/worker.js
@@ -0,0 +1,23 @@
+import amqp from 'amqplib'
+import { processTask } from '../p2p_hashsum_cracker/processTask.js'
+
+async function main() {
+	const connection = await amqp.connect('amqp://localhost')
+	const channel = await connection.createChannel()
+	const { queue } = await channel.assertQueue('tasks_queue')
+	channel.consume(queue, async (rawMessage) => {
+		const found = processTask(
+			JSON.parse(rawMessage.content.toString())
+		)
+		if (found) {
+			console.log(`Found! => ${found}`)
+
+			await channel.sendToQueue('results_queue',
+				Buffer.from(`Found: ${found}`))
+		}
+
+		await channel.ack(rawMessage)
+	})
+}
+
+main().catch(err => console.error(err))
